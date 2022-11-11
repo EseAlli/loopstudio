3243e2c99c778ac5f14c77a502fddb45ed9d5cd7
EseAlli <essealli.ea@gmail.com>
2022-11-11 22:33:45 +0100

    header desktop view

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const toggleMenu = document.querySelector(".menu-control");
 const header = document.querySelector("header");
+const body = document.querySelector("body");
 
 toggleMenu.addEventListener("click", () => {
   console.log("clicked");
   if (!header.classList.contains("show-menu")) {
     header.className = "show-menu";
+    body.style.overflow = "hidden";
   } else {
     header.className = "";
+    body.style.overflow = "unset";
   }
 });
